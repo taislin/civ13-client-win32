c0fcd5736c481f89237c07ed0b0dbe609a72db18
Taislin <cariosecus@gmail.com>
2021-03-05 14:41:22 +0000

    first commit

---
+++ tools/init.bat
@@ -0,0 +1,11 @@
+call "./git/bin/git.exe" init ./../resources/app/
+cd ./../resources/app/
+call "./../../tools/git/bin/git.exe" config core.sparseCheckout true
+cd .git
+cd info
+echo resources/ > sparse-checkout
+cd..
+cd..
+call "./../../tools/git/bin/git.exe" remote add origin https://github.com/civ13/civ13-typespess
+call "./../../tools/git/bin/git.exe" fetch --depth=1 origin master
+call "./../../tools/git/bin/git.exe" checkout master
