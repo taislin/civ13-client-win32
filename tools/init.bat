ef50dc6919d5617b080d99ea4141596603be256c
Taislin <cariosecus@gmail.com>
2021-03-11 14:27:04 +0000

    reworking some files, adding launcher

--- tools/init.bat
+++ tools/init.bat
@@ -1,5 +1,9 @@
-call "./git/bin/git.exe" init ./../resources/app/
-cd ./../resources/app/
+call "./7za/7za.exe" x git.7z -y
+cd..
+call "./tools/7za/7za.exe" x src.7z -y
+call "./tools/7za/7za.exe" x resources.7z -y
+cd resources/app/
+call "./../../tools/git/bin/git.exe" init
 call "./../../tools/git/bin/git.exe" config core.sparseCheckout true
 cd .git
 cd info
@@ -9,5 +13,16 @@ cd..
 call "./../../tools/git/bin/git.exe" remote add origin https://github.com/civ13/civ13-typespess
 call "./../../tools/git/bin/git.exe" fetch --depth=1 origin master
 call "./../../tools/git/bin/git.exe" checkout master
+call "./../../tools/git/bin/git.exe" reset --hard origin/master
 cd resources/
-call curl https://raw.githubusercontent.com:443/Civ13/typespess-client/main/client.js -O client.js
+call "./../../../tools/git/bin/git.exe" init
+call "./../../../tools/git/bin/git.exe" config core.sparseCheckout true
+cd .git
+cd info
+echo client.js > sparse-checkout
+cd..
+cd..
+call "./../../../tools/git/bin/git.exe" remote add origin https://github.com/civ13/typespess-client
+call "./../../../tools/git/bin/git.exe" fetch --depth=1 origin main
+call "./../../../tools/git/bin/git.exe" checkout main
+call "./../../../tools/git/bin/git.exe" reset --hard origin/main
--- tools/update.bat
+++ tools/update.bat
@@ -1,10 +1,10 @@
 if not exist "./../resources/app/initialized" (
   echo initialized > ./../resources/app/initialized
-  call "./init.bat"
+  call "./init.bat" >nul
 )
 cd..
 cd resources/app/
 call "./../../tools/git/bin/git.exe" pull
 cd resources/
-call curl https://raw.githubusercontent.com:443/Civ13/typespess-client/main/client.js -O ./../resources/app/resources/client.js
+call "./../../../tools/git/bin/git.exe" pull
 echo finished updating.
